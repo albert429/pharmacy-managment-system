afaf65da76157ce4d8806d498319ec8a611792df
albert429 <albertalfred429@gmail.com>
2026-03-05 20:48:24 +0200

    Configuring SPA

---
+++ controller/medicineController.js
@@ -0,0 +1,30 @@
+app.controller('MedicineController', function ($scope, PharmacyService) {
+  $scope.medicines = [];
+  $scope.newMedicine = {};
+
+  $scope.getMedicines = function () {
+    PharmacyService.getMedicines().then(
+      function (response) {
+        $scope.medicines = response.data;
+      },
+      function (error) {
+        console.error('Error fetching medicines:', error);
+      }
+    );
+  };
+
+  $scope.addMedicine = function () {
+    PharmacyService.addMedicine($scope.newMedicine).then(
+      function (response) {
+        $scope.medicines.push(response.data);
+        $scope.newMedicine = {};
+      },
+      function (error) {
+        console.error('Error adding medicine:', error);
+      }
+    );
+  };
+
+  // Initialize by fetching medicines
+  $scope.getMedicines();
+});
